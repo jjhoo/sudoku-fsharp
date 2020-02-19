c92f315bd247b152509e3b9677e8e578a8b87cf6
Jani Hakala <jjhakala@gmail.com>
2020-02-20 00:13:49 +0200

    Migrate to dotnet

---
+++ Sudoku/Script.fsx
@@ -0,0 +1,10 @@
+﻿#load "Combinations.fs"
+#load "Permutations.fs"
+#load "Types.fs"
+#load "Solver.fs"
+
+open Sudoku.Solver
+
+let strGrid = "700600008800030000090000310006740005005806900400092100087000020000060009600008001"
+let solver = Solver(strGrid)
+solver.Solve() |> ignore
